4887904e414dd08f5d8d1a56cff77ee3a0f826e1
Maurizio La Rosa <maurizio.larosa@unime.it>
2021-11-03 17:34:34 +0100

    Update olives.R: add third plot

--- olives.R
+++ olives.R
@@ -17,13 +17,19 @@ oil <- transform(oil,
 oil_table <- data.frame(format(as.Date(oil$date,format="%y-%m-%d"),"%d/%m/%Y"),paste(oil$olives_sp+oil$olives_f," Kg"),paste(oil$production," Kg"),paste(oil$oil_return*100," %"),paste(oil$prod_litre," Lt"),paste("EUR ",oil$tot_cost))
 colnames(oil_table) <- c("Data","Raccolto","Olio prodotto (Kg)","Resa","Olio prodotto (Lt)","Costo")
 
-par(mfrow=c(1,2),mai=c(1,.4,1,.4))
-oil_plot <- barplot(cbind(oil$olives_sp,oil$olives_f)~oil$date,beside=TRUE,ylim=c(0,150),col=c("#00A600","#EAB64E"),xaxt="n",xlab="",ylab="Kg",main="Olive raccolte (Kg)")
+par(mfrow=c(1,3),mai=c(1,.4,1,.4))
+oil_plot <- barplot(cbind(oil$olives_sp,oil$olives_f)~oil$date,beside=TRUE,ylim=c(0,200),col=c("#00A600","#EAB64E"),xaxt="n",xlab="",ylab="Kg",main="Olive raccolte (Kg)")
 axis(1,at=c(2,5),labels=oil$date)
 text(x=t(oil_plot),y=cbind(oil$olives_sp,oil$olives_f),labels=cbind(paste(c(oil$olives_sp,oil$olives_f)," Kg")),pos=3,cex=.8) #note that I transpose the plot coordinates (t(oil_plot)), for the bar labels to be correct (they would be mixed up, otherwise)
-legend(1,140,c("San Placido","Foddiri"),fill=c("#00A600","#EAB64E"),cex=.85)
+legend(1,180,c("San Placido","Foddiri"),fill=c("#00A600","#EAB64E"),cex=.85)
 
-oil_plot2 <- barplot(cbind(sum(oil$olives_sp),sum(oil$olives_f)),beside=TRUE,space=c(1,.4),ylim=c(0,200),col=c("#00A600","#EAB64E"))
+oil_plot2 <- barplot(cbind(sum(oil$olives_sp),sum(oil$olives_f)),beside=TRUE,space=c(1,.4),ylim=c(0,200),col=c("#00A600","#EAB64E"),main="Olive raccolte per campagna (Kg)")
 axis(1,at=oil_plot2,labels=c("San Placido","Foddiri"))
 text(x=oil_plot2,y=cbind(sum(oil$olives_sp),sum(oil$olives_f)),labels=paste(cbind(sum(oil$olives_sp),sum(oil$olives_f))," Kg"),pos=3,cex=.9)
 #legend(.4,185,c("San Placido","Foddiri"),fill=c("#00A600","#EAB64E"),cex=.85)
+
+product_matrix <- cbind(oil$olives_sp+oil$olives_f,oil$production) #I put olives harvested by date and oil produced (in chilos) in a matrix
+product_plot <- barplot(product_matrix~oil$date,beside=TRUE,ylim=c(0,200),col=c("#ABC32F","#207567"),xaxt="n",xlab="",main="Olio prodotto per raccolto (Kg)") #I plot that matrix on date
+axis(1,at=c(2,5),labels=oil$date)
+text(x=product_plot,y=t(product_matrix),labels=paste(t(product_matrix)," Kg"),pos=3)
+legend(x=3.5,y=190,c("Olive","Olio"),fill=c("#ABC32F","#207567"),cex=.85)
